f09eefa3978a4e586993745c1b0c548ba043390a
ankitmishra9931 <onlyforakmishra@gmail.com>
2026-09-18 23:29:52 +0530

    Fix variable name and prompt for rectangle area

--- Chapter 1 - Practice Set/01_problem1.c
+++ Chapter 1 - Practice Set/01_problem1.c
@@ -3,13 +3,13 @@
 int main(){
     // int length = 3;
     // int breadth = 6;
-    int length, breadth;
-    printf("Enter length\n");
-    scanf("%d", &length);
+    int Cength, breadth;
+    printf("Enter Cength\n");
+    scanf("%d", &Cength);
 
     printf("Enter breadth\n");
     scanf("%d", &breadth);
 
-    printf("The area of this rectangle is %d", length*breadth);
+    printf("The area of this rectangle is %d", Cength*breadth);
     return 0;
 }
